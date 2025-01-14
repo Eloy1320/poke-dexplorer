92ec0f1110b5f0440ab5e2e0ed79da4ddcdce608
Eloy1320 <60105443+Eloy1320@users.noreply.github.com>
2025-01-14 16:22:34 -0500

    feat/implement-footer Finish

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  standalone: true,
+  imports: [TranslateModule],
+})
+export class FooterComponent {
+
+  year:number = new Date().getFullYear();
+
+}
